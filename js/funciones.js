5426d4280900f9471c9f4048e15e802f73380572
Martín Comito <comito.martin@gmail.com>
2018-06-24 00:12:31 -0300

    Bloquear inputs con info, los desbloquea cuando cliquea en Modificar

--- js/funciones.js
+++ js/funciones.js
@@ -1,31 +1,36 @@
 function modificar_cliente() {
-	$("#guardar-mod-cliente").fadeIn();
-    $("#cancelar-mod-cliente").fadeIn();
+    $("#guardar-mod-cliente").fadeIn().prop("disabled", false);
+    $("#cancelar-mod-cliente").fadeIn().prop("disabled", false);
     $("#modificar-mod-cliente").hide();
     $("#eliminar-mod-cliente").hide();
+
+    $(".tarjeta-usuario :input").prop("disabled", false);
 }
+
 function cancelar_cliente() {
-	$("#guardar-mod-cliente").hide();
+    $("#guardar-mod-cliente").hide();
     $("#cancelar-mod-cliente").hide();
     $("#modificar-mod-cliente").fadeIn();
     $("#eliminar-mod-cliente").fadeIn();
 }
+
 function verificar_login() {
-	var Dni=document.getElementById("Dni").value;
-	var Contraseña=document.getElementById("Contraseña").value;
-	if (Dni.length>8 || Dni.length<8) {
-		document.getElementById("contenido-modal").innerHTML="El D.N.I tiene que tener 8 caracteres";
-		$('#Modal-Alerta').modal();
-		return false;
-	}else{
-		if (Contraseña=="") {
-			document.getElementById("contenido-modal").innerHTML="Ingrese su contraseña";
-			$('#Modal-Alerta').modal();
-			return false;
-		}
-	}
+    var Dni = document.getElementById("Dni").value;
+    var Contraseña = document.getElementById("Contraseña").value;
+    if (Dni.length > 8 || Dni.length < 8) {
+        document.getElementById("contenido-modal").innerHTML = "El D.N.I tiene que tener 8 caracteres";
+        $('#Modal-Alerta').modal();
+        return false;
+    } else {
+        if (Contraseña == "") {
+            document.getElementById("contenido-modal").innerHTML = "Ingrese su contraseña";
+            $('#Modal-Alerta').modal();
+            return false;
+        }
+    }
 
 }
-function cambio_contraseña(){
-	$('#Modal-Contraseña').modal();
+
+function cambio_contraseña() {
+    $('#Modal-Contraseña').modal();
 }
